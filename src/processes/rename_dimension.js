8845ac998b7ec7318eef3f0f9b895f311e099585
bgoesswe <bernhard.goesswein@geo.tuwien.ac.at>
2020-02-14 09:10:03 +0100

    updated rename_dimension process

--- src/processes/rename_dimension.js
+++ src/processes/rename_dimension.js
@@ -5,26 +5,26 @@ module.exports = class rename_dimension extends BaseProcess {
 
 	async execute(node) {
 		var dc = node.getData("data");
-		var oldName = node.getArgument('old');
-		var newName = node.getArgument('new');
+		var srcName = node.getArgument('source');
+		var trgName = node.getArgument('target');
 
-		if (dc.hasDimension(oldName)) {
+		if (dc.hasDimension(srcName)) {
 			throw new Errors.ProcessArgumentInvalid({
 				process: this.schema.id,
-				argument: 'old',
+				argument: 'source',
 				reason: 'A dimension with the specified name does not exist.'
 			});
 		}
 
-		if (dc.hasDimension(newName)) {
+		if (dc.hasDimension(trgName)) {
 			throw new Errors.ProcessArgumentInvalid({
 				process: this.schema.id,
-				argument: 'new',
+				argument: 'target',
 				reason: 'A dimension with the specified name already exists.'
 			});
 		}
 
-		dc.renameDimension(oldName, newName);
+		dc.renameDimension(srcName, trgName);
 		return dc;
 	}
 
